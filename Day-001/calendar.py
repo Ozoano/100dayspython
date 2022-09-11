b696ee9ccfef7669aebe4dc56ffde000a54ae38a
Ozoano <106312924+Ozoano@users.noreply.github.com>
2022-09-11 16:38:21 +0100

    Create calendar.py
    calendar displaying a month, and dates

---
+++ Day-001/calendar.py
@@ -0,0 +1,17 @@
+# Library import
+import calendar
+
+#print("Enter Year: ", end = ' ')
+try:
+    yy = int(input('Enter year: '))
+    print("\nEnter Month Number (1-12): ", end="")
+    try:
+        mm = int(input())
+        if mm>=1 and mm<=12:
+            print("\n", calendar.month(yy, mm))
+        else:
+            print("\nInvalid Month Number!")
+    except ValueError:
+        print("\nInvalid Input!")
+except ValueError:
+    print("\nInvalid Input!")
